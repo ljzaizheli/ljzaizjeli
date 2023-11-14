3c9c8161bd9f1ac20936e89c57107e25a66bc710
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-11-14 19:58:24 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main()
-{
-	cout << "char length:" << sizeof(char) << endl;
-	cout << "int length:" << sizeof(int) << endl;
-	cout << "bool length:" << sizeof(bool) << endl;
-	cout << "float length:" << sizeof(float) << endl;
-	cout << "double length:" << sizeof(double) << endl;
+int main() {
+	unsigned int testUnint = 65534;
+	cout << "output in unsigned int 1 type:" << testUnint<< endl;
+		cout << "output in char type:" << static_cast<char>(testUnint) << endl;
+	cout << "output in short type:" << static_cast<short>(testUnint) << endl;
+	cout << "output in int type:" << static_cast<int>(testUnint) << endl;
+	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
+	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnint) << endl;
+	cout << "output in Hex unsigned int type:" << hex << testUnint << endl; 
+	system("pause");
+	return 0;
 }
