40234092753396bada7497e8e596400b7698d432
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-11-14 23:50:06 +0800

    Add files via upload

---
+++ 实验2七.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	int i = 0, j;
+	for (i;i < 5; i++) {
+		for (j = 0;j <= i;j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	
+	}
+
+
+
+
+
+
+
+	return 0;
+}
