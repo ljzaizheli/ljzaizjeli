69e4f485018dd41daf0fa53aaf0fb412c133c09c
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-11-14 19:58:42 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-int main() {
-	unsigned int testUnint = 65534;
-	cout << "output in unsigned int 1 type:" << testUnint<< endl;
-		cout << "output in char type:" << static_cast<char>(testUnint) << endl;
-	cout << "output in short type:" << static_cast<short>(testUnint) << endl;
-	cout << "output in int type:" << static_cast<int>(testUnint) << endl;
-	cout << "output in double type:" << static_cast<double>(testUnint) << endl;
-	cout << "output in double type:" << setprecision(4) << static_cast<double>(testUnint) << endl;
-	cout << "output in Hex unsigned int type:" << hex << testUnint << endl; 
-	system("pause");
+int main()
+{
+	float a, b;
+	cout << "请输入华式温度:";
+	cin >> a;
+	b = (a - 32)*5/9;
+	cout << fixed << setprecision(2)<<"摄氏温度：" << b << endl;
 	return 0;
 }
